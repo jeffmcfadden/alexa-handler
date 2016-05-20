b435b4dedc283f6bae5479b659ff0f63b5cf8a2c
Jeff McFadden <jeff@forgeapps.com>
2016-05-20 14:20:46 -0700

    Redirect

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,9 @@
 class AuthController < ApplicationController
 
   def access_token
+    url = "#{params[:redirect_uri]}?access_token=#{ENV['AUTHORIZATION_TOKEN']}"
     
+    redirect_to url and return
   end
 
 end
