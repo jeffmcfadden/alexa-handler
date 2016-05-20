dc97098833010b3930c76f9541741f1cf0ea6b82
Jeff McFadden <jeff@forgeapps.com>
2016-05-20 14:25:52 -0700

    Add more params.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,9 @@ class AuthController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:access_token]
 
   def access_token
-    url = "#{params[:redirect_uri]}?access_token=#{ENV['AUTHORIZATION_TOKEN']}"
+    
+    # state=xyz&access_token=2YotnFZFEjr1zCsicMWpAA  
+    url = "#{params[:redirect_uri]}?access_token=#{ENV['AUTHORIZATION_TOKEN']}&token_type=Bearer"
     
     redirect_to url and return
   end
