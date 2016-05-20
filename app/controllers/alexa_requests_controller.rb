b1e764ab16b4246986da3f551b9fa573b0d079fd
Jeff McFadden <jeff@forgeapps.com>
2016-05-20 15:55:44 -0700

    Attempt to add some turn on and off handler events.

--- app/controllers/alexa_requests_controller.rb
+++ app/controllers/alexa_requests_controller.rb
@@ -12,41 +12,82 @@ class AlexaRequestsController < ApplicationController
     
     response_message_id = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
     
-    if true || params["header"]["namespace"] == "Alexa.ConnectedHome.Discovery"
-      data =  {
-          "header": {
-              "messageId": "#{response_message_id}",
-              "name": "DiscoverAppliancesResponse",
-              "namespace": "Alexa.ConnectedHome.Discovery",
-              "payloadVersion": "2"
-          },
-          "payload": {
-              "discoveredAppliances": [
-                  {
-                      "actions": [
-                          "turnOn",
-                          "turnOff"
-                      ],
-                      "additionalApplianceDetails": {},
-                      "applianceId": "kitchenLightsID1234",
-                      "friendlyDescription": "Kitchen Lights",
-                      "friendlyName": "Kitchen",
-                      "isReachable": true,
-                      "manufacturerName": "jeffMcFaddenManufacturer",
-                      "modelName": "fancyLight",
-                      "version": "1.0.0"
-                  }
-              ]
-          }
-      }
-    else
-      data = {}
-    end
+    I, [2016-05-20T15:49:08.304800 #3195]  INFO -- :   Parameters: {"header"=>{"namespace"=>"Alexa.ConnectedHome.Control", "name"=>"TurnOnRequest", "payloadVersion"=>"2", "messageId"=>"965a3eed-b78b-40e3-b50e-3c5de6db0e42"}, "payload"=>{"accessToken"=>"hPjjianfrBwXA8EkafAP6Hd", "appliance"=>{"applianceId"=>"kitchenLightsID1234", "additionalApplianceDetails"=>{}}}, "alexa_request"=>{"header"=>{"namespace"=>"Alexa.ConnectedHome.Control", "name"=>"TurnOnRequest", "payloadVersion"=>"2", "messageId"=>"965a3eed-b78b-40e3-b50e-3c5de6db0e42"}, "payload"=>{"accessToken"=>"hPjjianfrBwXA8EkafAP6Hd", "appliance"=>{"applianceId"=>"kitchenLightsID1234", "additionalApplianceDetails"=>{}}}}}
     
+    
+    if params["header"]["namespace"] == "Alexa.ConnectedHome.Discovery"
+      data =  data_for_discovery_request
+    elsif params["header"]["namespace"] == "Alexa.ConnectedHome.Control" && params["header"]["name"] == "TurnOnRequest"
+      `/usr/local/bin/zway kitchen_lights on`
+      data = data_for_turn_on_request
+    elsif params["header"]["namespace"] == "Alexa.ConnectedHome.Control" && params["header"]["name"] == "TurnOffRequest"
+      `/usr/local/bin/zway kitchen_lights off`
+      data = data_for_turn_off_request
+    end
     
     respond_to do |format|
       format.json { render :json => data }
     end
   end
+  
+  def data_for_discovery_request
+    response_message_id = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    
+    {
+        "header": {
+            "messageId": "#{response_message_id}",
+            "name": "DiscoverAppliancesResponse",
+            "namespace": "Alexa.ConnectedHome.Discovery",
+            "payloadVersion": "2"
+        },
+        "payload": {
+            "discoveredAppliances": [
+                {
+                    "actions": [
+                        "turnOn",
+                        "turnOff"
+                    ],
+                    "additionalApplianceDetails": {},
+                    "applianceId": "kitchenLightsID1234",
+                    "friendlyDescription": "Kitchen Lights",
+                    "friendlyName": "Kitchen",
+                    "isReachable": true,
+                    "manufacturerName": "jeffMcFaddenManufacturer",
+                    "modelName": "fancyLight",
+                    "version": "1.0.0"
+                }
+            ]
+        }
+    }
+  end
+  
+  def data_for_turn_on_request
+    response_message_id = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    
+    {
+        "header": {
+            "messageId": "#{response_message_id}",
+            "name": "TurnOnConfirmation",
+            "namespace": "Alexa.ConnectedHome.Control",
+            "payloadVersion": "2"
+        },
+        "payload": {}
+    }
+  end
+  
+  def data_for_turn_off_request    
+    response_message_id = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    
+    {
+        "header": {
+            "messageId": "#{response_message_id}",
+            "name": "TurnOffConfirmation",
+            "namespace": "Alexa.ConnectedHome.Control",
+            "payloadVersion": "2"
+        },
+        "payload": {}
+    }
+  end
+  
 
 end
