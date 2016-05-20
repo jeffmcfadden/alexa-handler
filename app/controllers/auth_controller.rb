1460cc794133a0d5099b201a503193f966e8a26a
Jeff McFadden <jeff@forgeapps.com>
2016-05-20 14:35:09 -0700

    Try this.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,7 +7,17 @@ class AuthController < ApplicationController
     # state=xyz&access_token=2YotnFZFEjr1zCsicMWpAA  
     url = "#{params[:redirect_uri]}?access_token=#{ENV['AUTHORIZATION_TOKEN']}&token_type=Bearer"
     
-    redirect_to url and return
+    data = {
+      "access_token": "#{ENV['AUTHORIZATION_TOKEN']}",
+      "token_type":"Bearer",
+      "expires_in":31533600,
+      "refresh_token": "#{ENV['REFRESH_TOKEN']}"
+    }
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => data }
+    end
   end
 
 end
